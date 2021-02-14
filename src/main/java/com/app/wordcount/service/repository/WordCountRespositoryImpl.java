238ebf17dff12aacb6a09618ea318fc3de54d149
mohan <mohanvamsi75@gmail.com>
2021-02-14 15:19:56 +0530

    Swagger Documentation Configuration, Code comments.

--- src/main/java/com/app/wordcount/service/repository/WordCountRespositoryImpl.java
+++ src/main/java/com/app/wordcount/service/repository/WordCountRespositoryImpl.java
@@ -1,10 +1,16 @@
 package com.app.wordcount.service.repository;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Data Access Layer of word-count service.
+ * @author "mohan vamsi <mohanvamsi75@gmail.com>"
+ */
+@Log4j2
 @Repository
 public class WordCountRespositoryImpl implements WordCountRespository{
 
@@ -17,18 +23,36 @@ public class WordCountRespositoryImpl implements WordCountRespository{
         valueOperations = redisTemplate.opsForValue();
     }
 
+    /**
+     * Method call to insert new key value pait to redis data-store.
+     * @param word
+     */
     @Override
     public void insertNewWord(String word) {
+        log.traceEntry();
         valueOperations.set(word, 1);
+        log.traceExit();
     }
 
+    /**
+     * Method call to get the value of key in redis data-store.
+     * @param word
+     * @return {@link Integer}
+     */
     @Override
     public Integer getWordCount(String word) {
-        return (Integer) valueOperations.get(word);
+        log.traceEntry();
+        return log.traceExit((Integer) valueOperations.get(word));
     }
 
+    /**
+     * Method call to increment value by 1 of a key in redis data-store.
+     * @param word
+     */
     @Override
     public void incrementWordCount(String word) {
+        log.traceEntry();
         valueOperations.increment(word);
+        log.traceExit();
     }
 }
--- src/main/java/com/app/wordcount/service/rest/api/WordCountServiceController.java
+++ src/main/java/com/app/wordcount/service/rest/api/WordCountServiceController.java
@@ -2,6 +2,9 @@ package com.app.wordcount.service.rest.api;
 
 import com.app.wordcount.service.models.WordCountResponse;
 import com.app.wordcount.service.serviceprovider.WordCountServiceDelegate;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+/**
+ * Class for handling REST endpoints for word-count service.
+ *
+ * @author "mohan vamsi <mohanvamsi75@gmail.com>"
+ */
 @RestController
+@Log4j2
+@Api("Rest endpoints for word-count service.")
 @RequestMapping("/api")
 public class WordCountServiceController {
 
@@ -19,18 +29,32 @@ public class WordCountServiceController {
 
     private HashMap<String, Integer> map = new HashMap<String, Integer>();
 
+    /**
+     * Rest Endpoint to get the count of a given word.
+     * @param word
+     * @return {@link ResponseEntity<WordCountResponse>}
+     */
+    @ApiOperation("Rest endpoint to get the count of a given word.")
     @GetMapping(path="/word/{wordVariable}")
     public ResponseEntity<WordCountResponse> getWordCount(@PathVariable("wordVariable") String word){
+        log.traceEntry("{}",word);
         Integer count = wordCountServiceDelegate.getWordCount(word);
-        return ResponseEntity.ok(new WordCountResponse(word, count));
+        return log.traceExit(ResponseEntity.ok(new WordCountResponse(word, count)));
     }
 
+    /**
+     * Rest Endpoint to inset a word.
+     * @param word
+     * @return {@link ResponseEntity}
+     */
+    @ApiOperation("Rest endpoint to insert a word.")
     @PutMapping(path="/word/{wordVariable}")
     public ResponseEntity putWord(@PathVariable("wordVariable") String word){
+        log.traceEntry("{}", word);
         wordCountServiceDelegate.insertWord(word);
-        return ResponseEntity
+        return log.traceExit(ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(String.format("%s in inserted", word));
+                .body(String.format("%s in inserted", word)));
     }
 }
